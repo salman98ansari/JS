a3d45457f265fdc488b3c80fb96178f32acb2e06
salman98ansari <salman98101@gmail.com>
2020-08-02 02:05:47 +0530

    variables

---
+++ Basic/variable.js
@@ -0,0 +1,37 @@
+/*
+//console.log("hello world!!!");
+var name = "salman";
+console.log(name)
+
+var job;
+console.log(job);
+// shows undefined
+
+var age =32;
+console.log(age);
+
+console.log(name + ' ' + age);
+
+ */
+
+var name ="salman";
+var age = "22";
+//type cohersion
+ console.log( name + " " + age);
+
+var job , ismarried ;
+job= "teacher";
+ismarried = false
+
+console.log(name + " is a "+ age + "year old boy"+ job + ". is he married ?"+ ismarried+".....")
+
+//variable mutation.
+
+age = "23"
+//it will update
+
+var lastname = prompt("what is your name ?");
+console.log(name + " " + lastname);
+
+
+
