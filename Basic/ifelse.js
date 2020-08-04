c5e8067553e6b0012d8d2a712c7a0b897baac69b
salman98ansari <salman98101@gmail.com>
2020-08-05 02:16:45 +0530

    functions

--- Basic/ifelse.js
+++ Basic/ifelse.js
@@ -6,3 +6,35 @@ if (num < 10 ){
 }else{
     console.log("unexpected")
 }
+
+//ternary and switch
+
+var num =10;
+num >= 9 ? console.log("greater") : console.log("smaller");
+
+var answer = num >= 9 ? 'greater' : 'smaller';
+console.log(answer);
+
+//switch
+
+var job = prompt("job bol ? engg yaa dr ");
+
+switch(job){
+	case 'engg':
+	console.log("u are engg");
+	break;
+	case 'dr':
+	console.log("u are dr");
+	break;
+}
+
+// falsy values : undefined , 0 , null , nan , '';
+ var height
+ if(height){
+ 	console.log("declare");
+ }
+ else{
+ 	console.log("undeclare");
+ }
+
+ // use === to compare bc
